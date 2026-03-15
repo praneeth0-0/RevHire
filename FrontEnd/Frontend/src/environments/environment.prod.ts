35795ac46cb3e6d95943f67124278112f6afaea4
praneeth0-0 <85997463+praneeth0-0@users.noreply.github.com>
2026-03-15 20:25:35 +0530

    Update API URL for production environment

--- FrontEnd/Frontend/src/environments/environment.prod.ts
+++ FrontEnd/Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:9090/api' // Change this for production deployment
+    apiUrl: 'http://16.112.108.236:8080/api'
 };
